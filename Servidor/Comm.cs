89830eb09d61c4f93e67ce18f71a8f4ec15593cd
Pedro T. R. Pinheiro <prppedro@gmail.com>
2014-10-23 07:11:52 -0200

    Servidor.DLL
    Adicionando servidor.

---
+++ Servidor/Comm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation; 
+
+namespace Servidor
+{
+    class Comm
+    {
+        int porta;
+        IPAddress IP;
+        Random randomizador = new Random();
+
+        int AbreSoquete() 
+        {
+            try 
+            {
+                porta = randomizador.Next(12400, 12422);
+                determinaIP(); 
+                Socket ouvidor = new TcpListener(IP, porta).Server;
+
+                //FAZER A LÓGICA
+                return porta;
+            }
+            catch (Exception ex) 
+            {
+                throw ex; 
+            }
+        }
+
+
+        private void determinaIP()
+        {
+            //Usa o IP da LAN, definindo o a variável IP da classe
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    IP = ip; 
+                }
+            }
+        }
+
+
+    }
+}
