3ecbe8d89d8c2aa07b62081b74586dc46dc305ec
Pedro T. R. Pinheiro <prppedro@gmail.com>
2014-10-23 03:03:31 -0200

    Questão de TOC
    A classe JogadorRemoto estava faltando.

---
+++ LANudo/LANudo/JogadorRemoto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace LANudo
+{
+    class JogadorRemoto : Jogador
+    {
+        int id();
+        Color Cor();
+        int Rotacao();
+        int Vez();
+        //void SuaVez(); 
+        bool EhVez();
+        Peao[] Peoes();
+        int PeoesCasaFinal();
+    }
+}
